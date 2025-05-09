f3bb3f3c538d1c889d2e3194c7b0b115ee2dfa30
Anay Saha <anaysahapaban@gmail.com>
2025-05-10 03:11:12 +0600

    lottie

---
+++ src/router/router.jsx
@@ -0,0 +1,27 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "./Home";
+import Register from "../pages/Register/Register";
+
+  const router = createBrowserRouter([
+    {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+
+      {
+        path: 'register',
+        element: <Register></Register>
+      }
+    ]
+    },
+    ]);
+    
+    export default router;
