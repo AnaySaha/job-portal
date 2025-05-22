5373b3f76f8117907bbb20a78f2fd552f52d3e16
Anay Saha <anaysahapaban@gmail.com>
2025-05-23 03:35:12 +0600

    error

--- src/JobApply/ApplyJob.jsx
+++ src/JobApply/ApplyJob.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
 
 const ApplyJob = () => {
-const {id} = useParams();
-console.log(id);
+const { id } = useParams();
+const { user } = useAuth();
+// console.log(id, user);
 
 
 const submitJobApplication = e => {
@@ -13,22 +16,35 @@ const submitJobApplication = e => {
     const github = form.github.value;
     const resume =  form.resume.value;
 
-    console.log(linkedIn, github,resume)
+    // console.log(linkedIn, github, resume);
+
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedIn,
+      github,
+      resume
+    }
+
+    fetch('http://localhost:5000/job-applications', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(jobApplication)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
 }
 
 
     return (
         <div>
-           <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Apply now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-    </div>
+      
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <h1 className="text-5xl font-bold text-center"> Apply Job and Good Luck!</h1>
       <form onSubmit={submitJobApplication} className="card-body">
         <div className="form-control">
           <label className="label">
@@ -56,8 +72,7 @@ const submitJobApplication = e => {
       </form>
     </div>
   </div>
-</div>
-        </div>
+
     );
 };
 
