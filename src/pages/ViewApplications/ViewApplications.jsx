ef738a971781bc159865affe6e0f4199911a2adb
Anay Saha <anaysahapaban@gmail.com>
2025-06-04 03:42:57 +0600

    added status

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -1,46 +1,82 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
-    const ViewApplications = () => {
-        const applications = useLoaderData();
-    
-    return (
-        <div>
-            <h2 className="text-3xl">Applications for this job: {applications.length}</h2>
-        
-            <table className="table">
-    {/* head */}
+const ViewApplications = () => {
+  const applications = useLoaderData();
 
 
-    <thead>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <th>Email</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
+  const handleStatusUpdate = (e, id) => {
+    console.log(e.target.value, id)
 
-        {
-            applications.map((app, index) => <tr key= {app._id}>
+    const data = {
+      status: e.target.value
+    }
+    fetch(`http://localhost:5000/job-applications/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+  }
 
-            <th>{index + 1 }</th>
-            <td>{app.applicant_email}</td>
+  return (
+    <div>
+      <h2 className="text-3xl">Applications for this job: {applications.length}</h2>
+
+      <table className="table">
+        {/* head */}
+
+
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Update Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {
+            applications.map((app, index) => <tr key={app._id}>
+
+              <th>{index + 1}</th>
+              <td>{app.applicant_email}</td>
+              <td>Quality Control Specialist</td>
+              <td>
+                {/* xs */}
+                <select 
+                onChange={(e) => handleStatusUpdate(e, app._id)}
+                defaultValue={app.status || 'Change Status'}
+                
+                className="select select-bordered select-xs w-full max-w-xs">
+                  <option disabled>Change Status</option>
+                  <option>Under Review</option>
+                  <option>Set Interview</option>
+                  <option>Hired</option>
+                  <option>Rejected</option>
+                </select>
+
+              </td>
 
 
             </tr>)
-        }
+          }
 
         </tbody>
-        </table>
-        </div>
-    );
+      </table>
+    </div>
+  );
 };
 
 export default ViewApplications;
